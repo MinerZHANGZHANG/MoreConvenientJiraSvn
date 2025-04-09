be88a4db4bdc65fca05f1eeacba19a6baccddea3
ZZMiner <LeFantasqueNet@outlook.com>
2025-04-09 22:58:55 +0800

    Update control style & layout

---
+++ MoreConvenientJiraSvn.App/Converters/LevelToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using MoreConvenientJiraSvn.Core.Enums;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MoreConvenientJiraSvn.App.Converters;
+
+public class LevelToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is InfoLevel level)
+        {
+            return level switch
+            {
+                InfoLevel.Normal => Brushes.Green,
+                InfoLevel.Warning => Brushes.Orange,
+                InfoLevel.Error => Brushes.Red,
+                _ => Brushes.Gray
+            };
+        }
+
+        return Brushes.Gray; // Default color for unknown levels
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
