43cef932b56816272fd6f63b468caa3179df8c08
MinerZHANGZHANG <LeFantasqueNet@outlook.com>
2025-04-20 20:43:38 +0800

    update analysis window viewModel

--- MoreConvenientJiraSvn.App/ViewModels/Windows/IssueAIAnalysisViewModel.cs
+++ MoreConvenientJiraSvn.App/ViewModels/Windows/IssueAIAnalysisViewModel.cs
@@ -4,6 +4,7 @@ using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using MoreConvenientJiraSvn.Service;
+using MoreConvenientJiraSvn.Service.Plugins;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows;
@@ -13,47 +14,31 @@ namespace MoreConvenientJiraSvn.App.ViewModels
     public partial class IssueAIAnalysisViewModel(SemanticKernelService semanticKernelService, LogService logService) : ObservableObject, INotifyPropertyChanged
     {
         [ObservableProperty]
-        private string _promptText;
+        private string? _promptText;
 
         [ObservableProperty]
-        private string _inputText;
+        private string? _inputText;
 
         [ObservableProperty]
-        private string _outputText;
+        private string? _outputText;
 
         [ObservableProperty]
-        private string _selectedModel;
+        private string? _selectedModel;
 
         [ObservableProperty]
-        private string _aiServiceAddress;
+        private ObservableCollection<string> _models = [];
 
         [ObservableProperty]
-        private string _apiKey;
+        private string? _aiServiceAddress;
+
+        [ObservableProperty]
+        private string? _apiKey;
 
         [ObservableProperty]
         private bool _isStreamResponse;
 
-        public ObservableCollection<string> Models { get; } = new ObservableCollection<string>
-        {
-            "Model 1",
-            "Model 2",
-            "Model 3"
-        };
-
-        // New properties for multi-file selection in code问答.
-        public ObservableCollection<string> CodeFiles { get; } = new ObservableCollection<string>();
-
-        // New properties for multi-file selection in Sql问答.
-        public ObservableCollection<string> SqlFiles { get; } = new ObservableCollection<string>();
-
-        // New properties for folder file selection.
         [ObservableProperty]
-        private string _selectedFolderPath;
-
-        public ObservableCollection<string> FolderFiles { get; } = new ObservableCollection<string>();
-
-        [ObservableProperty]
-        private string _selectedFolderFile;
+        private ObservableCollection<IssueFile> _issueFiles = [];
 
         public void StartNewChat()
         {
@@ -71,9 +56,10 @@ namespace MoreConvenientJiraSvn.App.ViewModels
         [RelayCommand]
         public async Task AskQuestion()
         {
-            if (string.IsNullOrEmpty(InputText))
+            if (string.IsNullOrEmpty(InputText) || string.IsNullOrEmpty(AiServiceAddress)
+                || string.IsNullOrEmpty(SelectedModel) || string.IsNullOrEmpty(ApiKey))
             {
-                MessageBox.Show("Please enter a question.");
+                MessageBox.Show("Please enter a necessary info.");
                 return;
             }
 
