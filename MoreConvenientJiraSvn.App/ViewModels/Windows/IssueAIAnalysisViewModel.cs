a81061a46561bc223620b1664179144d5c4fdfb7
MinerZHANGZHANG <LeFantasqueNet@outlook.com>
2025-04-21 21:20:11 +0800

    [feat](AIAnalysis):Update viewModel according to ui.

--- MoreConvenientJiraSvn.App/ViewModels/Windows/IssueAIAnalysisViewModel.cs
+++ MoreConvenientJiraSvn.App/ViewModels/Windows/IssueAIAnalysisViewModel.cs
@@ -13,20 +13,7 @@ namespace MoreConvenientJiraSvn.App.ViewModels
 {
     public partial class IssueAIAnalysisViewModel(SemanticKernelService semanticKernelService, LogService logService) : ObservableObject, INotifyPropertyChanged
     {
-        [ObservableProperty]
-        private string? _promptText;
-
-        [ObservableProperty]
-        private string? _inputText;
-
-        [ObservableProperty]
-        private string? _outputText;
-
-        [ObservableProperty]
-        private string? _selectedModel;
-
-        [ObservableProperty]
-        private ObservableCollection<string> _models = [];
+        #region Model setting
 
         [ObservableProperty]
         private string? _aiServiceAddress;
@@ -38,8 +25,118 @@ namespace MoreConvenientJiraSvn.App.ViewModels
         private bool _isStreamResponse;
 
         [ObservableProperty]
+        private string? _selectedModel;
+
+        [ObservableProperty]
+        private ObservableCollection<string> _models = [];
+
+        [ObservableProperty]
+        private bool _enableMultiModalInput;
+
+        [ObservableProperty]
+        private ObservableCollection<string> _aiDefaultAccessedDirectory = [];
+
+        [ObservableProperty]
+        private string? _promptText;
+
+        public readonly IEnumerable<(string, string)> DefaultPromptList =
+        [
+            new (".NET代码分析","你是一位资深.NET开发人员，精通.NET6.0版本的WPF开发，并擅长分析代码，从中发现问题和提出优化建议"),
+            new ("Issue总结","你是一位资深的产品经理，你了解软件开发和用户交互，你擅长分析总结代码和相关的文档，并产出为测试和实施人员可以理解的内容"),
+        ];
+
+        #endregion
+
+        #region Current chat
+
+        [ObservableProperty]
         private ObservableCollection<IssueFile> _issueFiles = [];
 
+        [ObservableProperty]
+        private string? _issueKey;
+
+        [ObservableProperty]
+        private string? _inputText;
+
+        [ObservableProperty]
+        private string? _outputText;
+
+        #endregion
+
+        #region History chat
+
+        [ObservableProperty]
+        private DateTime _startDate;
+
+        [ObservableProperty]
+        private DateTime _endDate;
+
+        [ObservableProperty]
+        private ObservableCollection<ChatHistory> _chatHistories = [];
+
+        #endregion
+
+        #region Command
+
+        [RelayCommand]
+        public void AddAIDefaultAccessedDirectory()
+        {
+
+        }
+
+        [RelayCommand]
+        public void RemoveAIDefaultAccessedDirectory(string folderPath)
+        {
+
+        }
+
+        [RelayCommand]
+        public void AddAIAccessedFile()
+        {
+
+        }
+
+        [RelayCommand]
+        public void RemoveAIAccessedFile()
+        {
+
+        }
+
+        [RelayCommand]
+        public void ShowTipMessage(string message)
+        {
+
+        }
+
+        [RelayCommand]
+        public void LoadDefaultFilesByIssueKey()
+        {
+
+        }
+
+        [RelayCommand]
+        public void QueryChatHistories()
+        {
+
+        }
+
+        [RelayCommand]
+        public void SendQuestion()
+        {
+
+        }
+
+        #endregion
+
+        #region Help Function
+
+        public void SaveModelSetting()
+        {
+
+        }
+
+
+        #endregion
         public void StartNewChat()
         {
             // Initialize the chat history or any other necessary setup
