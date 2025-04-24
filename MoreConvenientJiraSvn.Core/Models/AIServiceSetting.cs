7b498a551122840809598a3e417c86c4dfff1c8b
MinerZHANGZHANG <LeFantasqueNet@outlook.com>
2025-04-24 20:51:19 +0800

    [fix](app start):fix background task block app start

---
+++ MoreConvenientJiraSvn.Core/Models/AIServiceSetting.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MoreConvenientJiraSvn.Core.Models;
+
+public record AIServiceSetting
+{
+    public string ServiceAddress { get; set; } = string.Empty;
+    public string ApiKey { get; set; } = string.Empty;
+    public string SelectedModel { get; set; } = string.Empty;
+    public ObservableCollection<string> Models { get; set; } = [];
+    public bool EnableStreamResponse { get; set; } = false;
+    public bool EnableMultiModalInput { get; set; } = false;
+    public ObservableCollection<string> DefaultAccessedDirectories { get; set; } = [];
+    public string PromptText { get; set; } = string.Empty;
+}
